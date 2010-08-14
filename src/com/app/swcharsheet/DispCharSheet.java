1dd167ce269dc594c9f205040c244793b6b2a43c
antaydos <antaydos@gmail.com>
2010-08-14 08:38:53 -0400

    Dialog reworking
    Doing dialogs as activities now, still need to get buttons to work.

--- src/com/app/swcharsheet/DispCharSheet.java
+++ src/com/app/swcharsheet/DispCharSheet.java
@@ -1,12 +1,16 @@
 package com.app.swcharsheet;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,9 +24,11 @@ import android.widget.TextView;
 public class DispCharSheet extends Activity {
 
 	characterSheet sheet;
-	Dialog  adjHPDialog,
-			adjXPDialog,
-			adjFPDialog;
+	
+	private int adjHP[], adjXP[];
+	
+	static final private int HPDialog = 0,
+							 XPDialog = 1;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,9 +38,8 @@ public class DispCharSheet extends Activity {
         int i;
         sheet = new characterSheet();
 
-    	adjHPDialog = new Dialog(this);
-    	adjXPDialog = new Dialog(this);
-    	//adjFPDialog = new Dialog(this);
+    	adjHP = new int[] {0,0,0,0};
+    	adjXP = new int[] {0,0,0,0};
         
         int[] displayFields = new int[] 
                              {R.id.dispCharName,   R.id.dispPlayerName,
@@ -93,19 +98,201 @@ public class DispCharSheet extends Activity {
 	    // Handle item selection
 	    switch (item.getItemId()) {
 	    case R.id.adjHP:
-	    	adjHPDialog.setContentView(R.layout.adjhp);
-	    	adjHPDialog.setTitle("Adjust HP");
-	    	adjHPDialog.show();
+	    	showDialog(HPDialog);
+	    	//adjHPInteractions();
 	    	return true;
 	    case R.id.adjXP:
-	    	adjXPDialog.setContentView(R.layout.adjxp);
-	    	adjXPDialog.setTitle("Adjust XP");
-	    	adjXPDialog.show();
+	    	showDialog(XPDialog);
 	    	return true;
 	    case R.id.adjFP:
 	    	return true;
 	    default:
 	        return super.onOptionsItemSelected(item);
 	    }
+	}
+
+	@Override
+	public Dialog onCreateDialog(int id){
+		AlertDialog.Builder temp = new AlertDialog.Builder(this);
+		LayoutInflater li = LayoutInflater.from(this);
+		View mView;
+		switch(id) {
+			case(HPDialog):
+				mView = li.inflate(R.layout.adjhp,null);
+				temp.setTitle("Adjust HP");
+				temp.setView(mView);
+	    		return temp.create();
+			case(XPDialog):
+				mView = li.inflate(R.layout.adjxp,null);
+				temp.setTitle("Adjust XP");
+				temp.setView(mView);
+		    	return temp.create();
+		    default:
+		    	return null;
+		}
+	}
+	@Override
+	public void onPrepareDialog(int id, Dialog dialog){
+		switch(id) {
+			case(HPDialog):
+				adjHPInteractions();
+				break;
+			case(XPDialog):
+	    		adjXPInteractions();
+				break;
+		}
+	}
+	
+	//TODO: Get these two methods to not cause a Force Close
+	private void adjHPInteractions(){
+		
+    	/*final Button incHPButton1000 = (Button) findViewById(R.id.incHP1000);
+    	incHPButton1000.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[0]++;
+    			populateField(R.id.adjHPText1000,Integer.toString(adjHP[0]));
+    		}
+    	});
+    	final Button incHPButton0100 = (Button) findViewById(R.id.incHP0100);
+    	incHPButton0100.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[1]++;
+    			populateField(R.id.adjHPText0100,Integer.toString(adjHP[1]));
+    		}
+    	});
+    	final Button incHPButton0010 = (Button) findViewById(R.id.incHP0010);
+    	incHPButton0010.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[2]++;
+    			populateField(R.id.adjHPText0010,Integer.toString(adjHP[2]));
+    		}
+    	});
+    	final Button incHPButton0001 = (Button) findViewById(R.id.incHP0001);
+    	incHPButton0001.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[3]++;
+    			populateField(R.id.adjHPText0001,Integer.toString(adjHP[3]));
+    		}
+    	});
+    	final Button decHPButton1000 = (Button) findViewById(R.id.decHP1000);
+    	decHPButton1000.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[0]--;
+    			populateField(R.id.adjHPText1000,Integer.toString(adjHP[0]));
+    		}
+    	});
+    	final Button decHPButton0100 = (Button) findViewById(R.id.decHP0100);
+    	decHPButton0100.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[1]--;
+    			populateField(R.id.adjHPText0100,Integer.toString(adjHP[1]));
+    		}
+    	});
+    	final Button decHPButton0010 = (Button) findViewById(R.id.decHP0010);
+    	decHPButton0010.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[2]--;
+    			populateField(R.id.adjHPText0010,Integer.toString(adjHP[2]));
+    		}
+    	});
+    	final Button decHPButton0001 = (Button) findViewById(R.id.decHP0001);
+    	decHPButton0001.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjHP[3]--;
+    			populateField(R.id.adjHPText0001,Integer.toString(adjHP[3]));
+    		}
+    	});
+    	
+    	final Button adjHPOK = (Button) findViewById(R.id.adjHPOK);
+    	adjHPOK.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			sheet.adjHP(0-convertToInt(adjHP));
+    		}
+    	});
+    	
+    	final Button adjHPCANCEL = (Button) findViewById(R.id.adjHPCANCEL);
+    	adjHPCANCEL.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			//adjHPDialog.dismiss();
+    		}
+    	});*/
+    	
+	}
+	
+	private void adjXPInteractions(){
+		/*final Button incXPButton1000 = (Button) findViewById(R.id.incXP1000);
+    	incXPButton1000.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[0]++;
+    			populateField(R.id.adjXPText1000,Integer.toString(adjXP[0]));
+    		}
+    	});
+    	final Button incXPButton0100 = (Button) findViewById(R.id.incXP0100);
+    	incXPButton0100.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[1]++;
+    			populateField(R.id.adjXPText0100,Integer.toString(adjXP[1]));
+    		}
+    	});
+    	final Button incXPButton0010 = (Button) findViewById(R.id.incXP0010);
+    	incXPButton0010.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[2]++;
+    			populateField(R.id.adjXPText0010,Integer.toString(adjXP[2]));
+    		}
+    	});
+    	final Button incXPButton0001 = (Button) findViewById(R.id.incXP0001);
+    	incXPButton0001.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[3]++;
+    			populateField(R.id.adjXPText0001,Integer.toString(adjXP[3]));
+    		}
+    	});
+    	final Button decXPButton1000 = (Button) findViewById(R.id.decXP1000);
+    	decXPButton1000.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[0]--;
+    			populateField(R.id.adjXPText1000,Integer.toString(adjXP[0]));
+    		}
+    	});
+    	final Button decXPButton0100 = (Button) findViewById(R.id.decXP0100);
+    	decXPButton0100.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[1]--;
+    			populateField(R.id.adjXPText0100,Integer.toString(adjXP[1]));
+    		}
+    	});
+    	final Button decXPButton0010 = (Button) findViewById(R.id.decXP0010);
+    	decXPButton0010.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[2]--;
+    			populateField(R.id.adjXPText0010,Integer.toString(adjXP[2]));
+    		}
+    	});
+    	final Button decXPButton0001 = (Button) findViewById(R.id.decXP0001);
+    	decXPButton0001.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			adjXP[3]--;
+    			populateField(R.id.adjXPText0001,Integer.toString(adjXP[3]));
+    		}
+    	});
+    	
+    	final Button adjXPOK = (Button) findViewById(R.id.adjXPOK);
+    	adjXPOK.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			sheet.adjXP(convertToInt(adjXP));
+    		}
+    	});
+    	
+    	final Button adjXPCANCEL = (Button) findViewById(R.id.adjXPCANCEL);
+    	adjXPCANCEL.setOnClickListener(new OnClickListener(){
+    		public void onClick(View v){
+    			//adjXPDialog.dismiss();
+    		}
+    	});*/
+	}
+	
+	private int convertToInt(int temp[]){
+		return temp[0]*1000+temp[1]*100+temp[2]*10+temp[3];
 	}
 }
